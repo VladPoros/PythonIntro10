45c6f7f917f9705a5969d1e2620b46ef05bf4d0d
Nikolay <kiseev.nikolay@gmail.com>
2021-01-24 10:26:15 +0200

    lesson_01

---
+++ lesson_01/first_programm.py
@@ -0,0 +1,34 @@
+# print('Hello World!', '45', 56.67)
+print(45)
+print(3.14)
+print(True)
+
+# long a = 45;      C - type
+a = 45
+b = a
+print(a, type(a), id(a))
+a = 'Hello'
+print(a, type(a), id(a))
+
+# value, type, ref_counter
+
+# int, float, complex
+# str, ESCAPE, RAW
+# boolean: True, False
+# list, tuple, range
+# set, frozenset
+# dict
+
+# int
+a = 2067508436543076543078654307564038564086543098564380256430856470154378056430756480715643870564308643098256047514
+print(a * 43756832765437265437265437268543276453267546735426354)
+print(0b10011101110101001)
+print(0o23432153465443543)
+print(0x32647532ABCDEF345)
+
+# float
+a = 3.14
+#   2.23 * 10 ^ -308            1.79 * 10 ^ +308
+b = 3 ** 10000
+print(b)
+c = b + 0.1
